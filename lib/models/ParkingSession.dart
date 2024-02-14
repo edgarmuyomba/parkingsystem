17ad6ed93937fd8ee1dfa26cc725c8bc36154921
edgarmuyomba <edgarmatthew247@gmail.com>
2024-02-14 22:19:30 +0300

    parking session changes

--- lib/models/ParkingSession.dart
+++ lib/models/ParkingSession.dart
@@ -1,6 +1,8 @@
 class ParkingSession {
   String uuid;
   String lot;
+  String lot_uuid;
+  String slot;
   String slot_number;
   int timestamp_start;
   int? timestamp_end;
@@ -10,6 +12,8 @@ class ParkingSession {
   ParkingSession(
       {required this.uuid,
       required this.lot,
+      required this.lot_uuid,
+      required this.slot,
       required this.slot_number,
       required this.timestamp_start,
       this.timestamp_end,
@@ -20,6 +24,8 @@ class ParkingSession {
     return ParkingSession(
         uuid: json['uuid'],
         lot: json['lot'],
+        lot_uuid: json['lot_uuid'],
+        slot: json['slot'],
         slot_number: json['slot_number'],
         timestamp_start: json['timestamp_start'],
         timestamp_end: json['timestamp_end'],
--- lib/presentation/user_sessions.dart
+++ lib/presentation/user_sessions.dart
@@ -32,13 +32,82 @@ class _UserSessionsState extends State<UserSessions> {
         .toList();
   }
 
+  void show_message(status_code, message) {
+    Color? bg_color = status_code == 200 ? Colors.green : Colors.red[400];
+    IconData icon = status_code == 200 ? Icons.check : Icons.fmd_bad_outlined;
+    String title = status_code == 200 ? "Success" : "Error";
+
+    double deviceHeight = MediaQuery.of(context).size.height;
+
+    showDialog(
+      context: context, 
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Container(
+            height: 0.2 * deviceHeight,
+            decoration: BoxDecoration(
+              color: bg_color,
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20
+                          ),
+                          ),
+                        Icon(
+                          icon,
+                          color: Colors.white,
+                          size: 25,
+                          )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      message,
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                      ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      });
+  }
+
+  Future<void> terminate_session() async {
+    var response = await http.post(Uri.parse(
+        "http://$server:8000/release_slot/${active_session!.lot_uuid}/${active_session!.slot}/$firebase_id/"));
+    var message = jsonDecode(response.body);
+    show_message(response.statusCode, message);
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     fetchUserSessions().then((value) {
-      parking_sessions = value;
-      if (parking_sessions[0].timestamp_end != null) {
+      setState(() {
+        parking_sessions = value;
+      });
+      if (parking_sessions[0].timestamp_end == null) {
         setState(() {
           is_active = true;
           active_session = parking_sessions[0];
@@ -55,151 +124,149 @@ class _UserSessionsState extends State<UserSessions> {
       ),
       body: Padding(
         padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Transform.scale(
-                          scale: 0.9,
-                          child: Image.asset("assets/active_car.png")),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Sheraton Hotel",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text("LV_1 1"),
-                          Text("Parked on"),
-                          SizedBox(
-                            height: 16,
-                          ),
-                          InkWell(
-                            onTap: null,
-                            child: Card(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(5)),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "End Session",
-                                    style: TextStyle(fontSize: 12),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (is_active) ...[
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    "Current Session",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                 ),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Table(
-                      border: TableBorder.symmetric(
-                        inside: BorderSide(width: 1.0, color: Colors.black26),
+                Container(
+                  width: double.infinity,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Transform.scale(
+                              scale: 1,
+                              child: Image.asset("assets/active_car.png")),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                active_session!.lot,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              Text(active_session!.slot_number),
+                              Text(active_session!.parked_on.split(', ')[0]),
+                              SizedBox(
+                                height: 16,
+                              ),
+                              InkWell(
+                                onTap: () => showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text("Terminate Session?"),
+                                      content: Text("Are you sure you want to end the current session?"),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () => Navigator.pop(context), 
+                                          child: Text(
+                                            "No",
+                                            style: TextStyle(
+                                              color: Colors.red
+                                            ),
+                                            )
+                                          ),
+                                        TextButton(
+                                          onPressed: () => {
+                                            Navigator.pop(context),
+                                            terminate_session()
+                                          }, 
+                                          child: Text(
+                                            "Yes",
+                                            style: TextStyle(
+                                              color: Colors.green
+                                            ),
+                                            )
+                                          )
+                                      ],
+                                    );
+                                  }
+                                ),
+                                child: Card(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(5)),
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "End Session",
+                                        style: TextStyle(fontSize: 12),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
                       ),
-                      defaultVerticalAlignment:
-                          TableCellVerticalAlignment.middle,
-                      children: [
-                        TableRow(
-                          children: [
-                            TableCell(
-                              child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Parking Lot',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                  ),
-                              ),
-                            ),
-                            TableCell(
-                              child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Slot',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                  ),
-                              ),
-                            ),
-                            TableCell(
-                              child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Date',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                  ),
-                              ),
-                            ),
-                            TableCell(
-                              child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Amount',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                  ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        TableRow(
-                          children: [
-                            TableCell(
-                              child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text('Data 1'),
-                              ),
-                            ),
-                            TableCell(
-                              child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text('Data 2'),
-                              ),
-                            ),
-                            TableCell(
-                              child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text('Data 3'),
-                              ),
-                            ),
-                            TableCell(
-                              child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text('Data 3'),
-                              ),
-                            ),
-                          ],
-                        ),
-                        // Add more rows as needed
-                      ],
-                    )),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+              ],
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  "Session History",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
               ),
-            )
-          ],
+              Container(
+                width: double.infinity,
+                child: Card(
+                  child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: DataTable(
+                          columns: [
+                            DataColumn(
+                                label: Expanded(
+                              child: Text("Parking Lot"),
+                            )),
+                            DataColumn(
+                                label: Expanded(
+                              child: Text("Slot"),
+                            )),
+                            // DataColumn(
+                            //     label: Expanded(
+                            //   child: Text("Date"),
+                            // )),
+                            DataColumn(
+                                label: Expanded(
+                              child: Text("Amount"),
+                            )),
+                          ],
+                          rows: parking_sessions.map((session) {
+                            return DataRow(cells: [
+                              DataCell(Text(session.lot)),
+                              DataCell(Text(session.slot_number)),
+                              // DataCell(Text(session.parked_on.split(', ')[0])),
+                              session.amount_accumulated != null
+                                  ? DataCell(Text(
+                                      session.amount_accumulated.toString()))
+                                  : DataCell(Text(
+                                      "Ongoing",
+                                      style: TextStyle(color: Colors.green),
+                                    )),
+                            ]);
+                          }).toList())),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
